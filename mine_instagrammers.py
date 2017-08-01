8462e610ec4937c869d8e059d479a9795e2bd7c2
Khanh Nguyen <khanhtnguyen@mavs.uta.edu>
2017-08-01 17:23:35 -0500

    made it to mine the rest

--- mine_instagrammers.py
+++ mine_instagrammers.py
@@ -5,31 +5,34 @@ import json
 if __name__ == '__main__':
   model = ResNet50(weights='imagenet')
 
-  with open('./influencers/aviation.txt') as f:
-    content = f.readlines()
-
-  instagrammers = [x.strip() for x in content]
-
-  tagFrequency = {}
-
-  for i in instagrammers:
-    print ('Tagging: ', i)
-
-    tags = analyzeInstagram(model, i)
-
-    for t in tags:
-      if t['tag'] not in tagFrequency:
-        tagFrequency[t['tag']] = {'count': 0, 'weightSum': 0}
-
-      tagFrequency[t['tag']]['count'] += 1
-      tagFrequency[t['tag']]['weightSum'] += t['weight']
+  todo = ['beauty_makeup', 'design', 'dogs', 'family', 'fashion', 'flower', 'food', 'healthy_living', 'illustration', 'lifestyle', 'music_artist', 'photographer', 'sports', 'travel', 'yoga']
   
-  tagArr = []
+  for to in todo:
+    with open('./influencers/' + to + '.txt') as f:
+      content = f.readlines()
 
-  for t in tagFrequency:
-    curObj = tagFrequency[t]
-    tagArr.append({'tag': t, 'weight': curObj['count']/(curObj['weightSum']/curObj['count'])})
-  
-  tagArr = sorted(tagArr, key=lambda k: k['weight'], reverse=False)
-  with open('./data/aviation.json', 'w') as j:
-      j.write(json.dumps(tagArr))
+    instagrammers = [x.strip() for x in content]
+
+    tagFrequency = {}
+
+    for i in instagrammers:
+      print ('Tagging: ', i)
+
+      tags = analyzeInstagram(model, i)
+
+      for t in tags:
+        if t['tag'] not in tagFrequency:
+          tagFrequency[t['tag']] = {'count': 0, 'weightSum': 0}
+
+        tagFrequency[t['tag']]['count'] += 1
+        tagFrequency[t['tag']]['weightSum'] += t['weight']
+    
+    tagArr = []
+
+    for t in tagFrequency:
+      curObj = tagFrequency[t]
+      tagArr.append({'tag': t, 'weight': curObj['count']/(curObj['weightSum']/curObj['count'])})
+    
+    tagArr = sorted(tagArr, key=lambda k: k['weight'], reverse=False)
+    with open('./data/' + to + '.json', 'w') as j:
+        j.write(json.dumps(tagArr))
