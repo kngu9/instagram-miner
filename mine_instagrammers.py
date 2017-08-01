3ea2e7db446aa0272f29b0fd5a29f1f452e8013f
Khanh Nguyen <khanhtnguyen@mavs.uta.edu>
2017-08-01 18:03:04 -0500

    Added exception handling

--- mine_instagrammers.py
+++ mine_instagrammers.py
@@ -17,22 +17,25 @@ if __name__ == '__main__':
 
     for i in instagrammers:
       print ('Tagging: ', i)
+      try:
+        tags = analyzeInstagram(model, i)
 
-      tags = analyzeInstagram(model, i)
+        for t in tags:
+          if t['tag'] not in tagFrequency:
+            tagFrequency[t['tag']] = {'count': 0, 'weightSum': 0}
 
-      for t in tags:
-        if t['tag'] not in tagFrequency:
-          tagFrequency[t['tag']] = {'count': 0, 'weightSum': 0}
+          tagFrequency[t['tag']]['count'] += 1
+          tagFrequency[t['tag']]['weightSum'] += t['weight']
+      
+        tagArr = []
 
-        tagFrequency[t['tag']]['count'] += 1
-        tagFrequency[t['tag']]['weightSum'] += t['weight']
-    
-    tagArr = []
+        for t in tagFrequency:
+          curObj = tagFrequency[t]
+          tagArr.append({'tag': t, 'weight': curObj['count']/(curObj['weightSum']/curObj['count'])})
+        
+        tagArr = sorted(tagArr, key=lambda k: k['weight'], reverse=False)
+        with open('./data/' + to + '.json', 'w') as j:
+            j.write(json.dumps(tagArr))
 
-    for t in tagFrequency:
-      curObj = tagFrequency[t]
-      tagArr.append({'tag': t, 'weight': curObj['count']/(curObj['weightSum']/curObj['count'])})
-    
-    tagArr = sorted(tagArr, key=lambda k: k['weight'], reverse=False)
-    with open('./data/' + to + '.json', 'w') as j:
-        j.write(json.dumps(tagArr))
+      except:
+        print ('User does not exist')
